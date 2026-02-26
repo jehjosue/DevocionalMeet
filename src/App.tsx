f3044f4e87efca69c1bb31cdca3dc12399b559f0
jefferson <domdefilmes@gmail.com>
2026-02-26 12:54:11 -0300

    feat: adição do RoomGuard em App.tsx para intercepção de rota de convites anônimos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router-dom";
 import Home from "./pages/Home";
 import Room from "./pages/Room";
 import { ThemeProvider } from "./context/ThemeContext";
+
+// Componente que redireciona /room/:roomName (sem ?nome=) para /?roomId=...
+function RoomGuard() {
+  const { roomName } = useParams();
+  const search = window.location.search;
+  const params = new URLSearchParams(search);
+
+  // Se já tem o nome, deixa entrar na sala
+  if (params.get("nome")) {
+    return <Room />;
+  }
+
+  // Se não tem nome, manda para a home com o roomId
+  return <Navigate to={`/?roomId=${roomName}`} replace />;
+}
 
 export default function App() {
   return (
@@ -14,7 +24,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/room/:roomName" element={<Room />} />
+          <Route path="/room/:roomName" element={<RoomGuard />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
